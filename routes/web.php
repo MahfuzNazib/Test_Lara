9353e83717f16038d47a844d48f09741afc36edf
MahfuzNazib <nazibmahfuz60@gmail.com>
2022-05-22 19:09:54 +0600

    New Code With Mail Sending

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PaypalController;
 use App\Http\Controllers\TestServiceController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,9 @@ Route::get('/', function () {
 
 Route::get('/test-service', [TestServiceController::class, 'index'])->name('test_service.index');
 Route::post('/submit-test', [TestServiceController::class, 'submit_test'])->name('test_service.submit');
+
+// For Paypal Routes
+Route::get('create-transaction', [PaypalController::class, 'createTransaction'])->name('createTransaction');
+Route::get('process-transaction', [PaypalController::class, 'processTransaction'])->name('processTransaction');
+Route::get('success-transaction', [PaypalController::class, 'successTransaction'])->name('successTransaction');
+Route::get('cancel-transaction', [PaypalController::class, 'cancelTransaction'])->name('cancelTransaction');
